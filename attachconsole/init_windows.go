d3e6a8444504b76ae1980943dadbe3b6cc1fdd0c
Minux <minux@golang.org>
2015-05-21 22:03:59 -0400

    attachconsole: add copyright header
    Signed-off-by: Minux <minux@golang.org>

--- attachconsole/init_windows.go
+++ attachconsole/init_windows.go
@@ -1,3 +1,7 @@
+// Copyright 2015 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // Package attachconsole is meant to be used as a side-effect only
 // import. It attaches the calling process to its parent's console.
 // It is most useful for an application built with
